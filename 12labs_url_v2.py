275dddd5674e9453b7e9244c481f65c779e57ce5
Sonam Gupta <sonamgupta1105@users.noreply.github.com>
2024-07-23 17:48:57 -0700

    Update 12labs_url_v2.py

--- 12labs_url_v2.py
+++ 12labs_url_v2.py
@@ -28,7 +28,7 @@ def create_index(client):
     if st.session_state['index_id'] is None:
         try:
             index = client.index.create(
-                name = "test_index_4",
+                name = "test_index_5",
                 engines=[
                     {
                         "name": "pegasus1.1",
